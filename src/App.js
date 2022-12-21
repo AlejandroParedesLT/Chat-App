e3502fe47ecf996a21f4f8a65a045d7959dd7c4b
Alejandro Paredes <alejocangrejo10@gmail.com>
2022-12-21 17:01:04 -0400

    Upload the finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+//import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { firebaseConfig } from './keys';
+//import { getAnalytics } from "firebase/analytics";
+
+
+
+//const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+//const db = getFirestore(app);
+//const analytics = getAnalytics(app);
+//const auth = firebase.auth();
+//const auth = getAuth();
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
+  const [user] = useAuthState(auth)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn(){
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+  }
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+}
+
+function SignOut(){
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom(){
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, { idField: 'id' });
+  const [formValue, setFormValue] = useState('');
+  const sendMessage = async(e) => {
+    e.preventDefault();
+    const {uid, photoURL} = auth.currentUser;
+    await messagesRef.add({
+      text:formValue,
+      createdAt:firebase.firestore.FieldValue.serverTimestamp(),
+      uid, 
+      photoURL,
+    })
+    setFormValue('');
+    dummy.current.scrollIntoView({behavior:'smooth'});
+  }
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+
+      </div>
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => this.setFormValue(e.target.value)} placeholder="say something nice" />
+        <button type='submit'>+</button>
+      </form>
+    </>
+  )
+}
+
+const ChatMessage = (props) => {
+  const {text, uid, photoURL} = props.message;
+  const messageClass = uid === auth.currentUser.uid ? 'sent':'received';
+  return (
+    <div className={`nessage ${messageClass}`}>
+      {/*<img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="" />*/}
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="" />
+      <p>{text}</p>
+    </div>
+  )
 }
 
 export default App;
